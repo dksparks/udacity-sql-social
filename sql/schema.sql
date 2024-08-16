cdad85d1439eb5f4964124a39a9b5e6c0b09172e
Doug Sparks <dksparks@gmail.com>
2024-08-15 23:20:23 -0400

    allow longer lines

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,9 +1,7 @@
 CREATE TABLE users (
   id BIGSERIAL PRIMARY KEY,
   username VARCHAR(25) UNIQUE NOT NULL,
-  CONSTRAINT username_nonempty CHECK (
-    LENGTH(TRIM(username)) > 0
-  )
+  CONSTRAINT username_nonempty CHECK (LENGTH(TRIM(username)) > 0)
 );
 
 CREATE TABLE topics (
@@ -11,24 +9,18 @@ CREATE TABLE topics (
   id SERIAL PRIMARY KEY,
   name VARCHAR(30) UNIQUE NOT NULL,
   description VARCHAR(500),
-  CONSTRAINT topic_name_nonempty CHECK (
-    LENGTH(TRIM(name)) > 0
-  )
+  CONSTRAINT topic_name_nonempty CHECK (LENGTH(TRIM(name)) > 0)
 );
 
 CREATE TABLE posts (
   id BIGSERIAL PRIMARY KEY,
-  user_id BIGINT REFERENCES users (id)
-    ON DELETE SET NULL,
-  topic_id INTEGER REFERENCES topics (id)
-    ON DELETE CASCADE,
+  user_id BIGINT REFERENCES users (id) ON DELETE SET NULL,
+  topic_id INTEGER REFERENCES topics (id) ON DELETE CASCADE,
   title VARCHAR(100) NOT NULL,
   -- choose a reasonable limit for url length
   url VARCHAR(2000),
   content TEXT,
-  CONSTRAINT title_nonempty CHECK (
-    LENGTH(TRIM(title)) > 0
-  ),
+  CONSTRAINT title_nonempty CHECK (LENGTH(TRIM(title)) > 0),
   CONSTRAINT post_url_absent_or_nonempty CHECK (
     url IS NULL OR LENGTH(TRIM(url)) > 0
   ),
@@ -44,38 +36,24 @@ CREATE TABLE posts (
 
 CREATE TABLE comments (
   id BIGSERIAL PRIMARY KEY,
-  user_id BIGINT REFERENCES users (id)
-    ON DELETE SET NULL,
+  user_id BIGINT REFERENCES users (id) ON DELETE SET NULL,
   content TEXT NOT NULL,
   -- top-level comments have a parent_post_id
-  parent_post_id BIGINT REFERENCES posts (id)
-    ON DELETE CASCADE,
+  parent_post_id BIGINT REFERENCES posts (id) ON DELETE CASCADE,
   -- other comments have a parent_comment_id
-  parent_comment_id BIGINT REFERENCES comments (id)
-    ON DELETE CASCADE,
-  -- comments must have a parent_post_id or
-  --   a parent_comment_id, but not both
+  parent_comment_id BIGINT REFERENCES comments (id) ON DELETE CASCADE,
+  -- comments must have a parent_post_id or a parent_comment_id, but not both
   CONSTRAINT comment_exactly_one_parent CHECK (
-    (
-      parent_post_id IS NULL
-      AND parent_comment_id IS NOT NULL
-    ) OR (
-      parent_post_id IS NOT NULL
-      AND parent_comment_id IS NULL
-    )
+    (parent_post_id IS NULL AND parent_comment_id IS NOT NULL) OR
+    (parent_post_id IS NOT NULL AND parent_comment_id IS NULL)
   ),
-  CONSTRAINT comment_content_nonempty CHECK (
-    LENGTH(TRIM(content)) > 0
-  )
+  CONSTRAINT comment_content_nonempty CHECK (LENGTH(TRIM(content)) > 0)
 );
 
 CREATE TABLE votes (
-  user_id BIGINT REFERENCES users (id)
-    ON DELETE SET NULL,
-  post_id BIGINT REFERENCES posts (id)
-    ON DELETE CASCADE,
-  -- it would be more efficient to use a boolean here,
-  --   but the project template suggests 1 and -1
+  user_id BIGINT REFERENCES users (id) ON DELETE SET NULL,
+  post_id BIGINT REFERENCES posts (id) ON DELETE CASCADE,
+  -- a boolean would be more efficient here, but the template suggests 1 and -1
   value SMALLINT NOT NULL,
   CONSTRAINT vote_up_or_down CHECK (value IN (1, -1)),
   PRIMARY KEY (post_id, user_id)
